f0f91329350423a76aba6c0cea946644909b1c98
Nitheesh <nitheeshks98@gmail.com>
2024-02-11 13:54:39 +0530

    favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
   return (
     <>
       <div className="h-[80px] font-sans mx-5 md:flex hidden justify-between items-center z-50">
-        <div className="z-50" onClick={()=> navigate('/')} >
+        <div className="z-50 cursor-pointer" onClick={()=> navigate('/')} >
           <img src="/images/campfly_logo.png" alt="campfly" />
         </div>
         <div className="flex lg:w-[40%] w-[40%] lg:justify-between justify-around items-center lg:px-20">
